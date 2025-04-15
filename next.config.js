2daf60134e0d6f507758fac76d21ff1ba138651b
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-04-14 23:22:21 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,23 +1,20 @@
-// next.config.js
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
     return [
       {
-        source: '/(.*)', // Apply to all routes
+        source: '/(.*)', // Apply headers to all routes
         headers: [
           {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.walletconnect.com https://*.cloudflare.com;
+              script-src 'self' 'unsafe-inline' https://*.walletconnect.com https://cdn.jsdelivr.net;
               style-src 'self' 'unsafe-inline';
               connect-src *;
               img-src * blob: data:;
               font-src 'self' https://fonts.gstatic.com;
-              frame-src *;
             `.replace(/\n/g, ''),
           },
         ],
