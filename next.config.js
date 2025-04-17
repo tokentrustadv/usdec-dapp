177920a2d7a1266f239284a4b5b15715224ef2f5
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-04-17 15:55:46 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' https://cdn.jsdelivr.net https://*.walletconnect.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://*.walletconnect.com;
               style-src 'self' 'unsafe-inline';
               connect-src *;
               img-src * data: blob:;
