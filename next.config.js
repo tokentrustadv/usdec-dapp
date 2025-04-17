ab8a2fd51d981226c26fbb64d6a913edc999eb19
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-04-17 15:31:03 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,33 +1,26 @@
-// next.config.js
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
           {
-            key: 'Content-Security-Policy',
+            key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://*.walletconnect.com;
-              style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://*.walletconnect.com https://*.cloudflare.com;
+              style-src 'self' 'unsafe-inline';
               connect-src *;
-              img-src * blob: data:;
+              img-src * data: blob:;
               font-src 'self' https://fonts.gstatic.com;
               frame-src *;
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-          {
-            key: 'Access-Control-Allow-Origin',
-            value: '*',
-          },
-        ],
-      },
+            `.replace(/\s{2,}/g, " ").trim()
+          }
+        ]
+      }
     ];
-  },
+  }
 };
 
 module.exports = nextConfig;
