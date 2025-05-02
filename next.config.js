8104572524cbc4d7ed1f37e9fb60311359d0a1a0
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-05-02 16:09:43 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-// next.config.js
-
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
@@ -10,12 +8,12 @@ const nextConfig = {
           {
             key: 'Content-Security-Policy',
             value:
-              "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://cdn.jsdelivr.net https://*.walletconnect.com https://*.cloudflare.com; style-src 'self' 'unsafe-inline'; connect-src *; img-src * data: blob:; font-src 'self' https://fonts.gstatic.com; frame-src *;"
-          }
-        ]
-      }
+              "default-src 'self'; script-src 'self' 'unsafe-inline' https://cdn.jsdelivr.net https://*.walletconnect.com; style-src 'self' 'unsafe-inline'; connect-src *; img-src * data: blob:; font-src 'self' https://fonts.gstatic.com; frame-src *;",
+          },
+        ],
+      },
     ];
-  }
+  },
 };
 
 module.exports = nextConfig;
