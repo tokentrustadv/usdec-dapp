825eedf12f2105f2b1945b29681c3753d64ee448
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-05-09 16:17:29 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,50 +1,23 @@
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { getDefaultConfig, RainbowKitProvider } from '@rainbow-me/rainbowkit';
+import { WagmiProvider, createConfig } from 'wagmi';
+import { http, createPublicClient } from 'viem';
+import { baseSepolia } from 'wagmi/chains';
 import '@rainbow-me/rainbowkit/styles.css';
 
-const { chains, publicClient } = configureChains(
-  [
-    {
-      id: 84532,
-      name: 'Base Sepolia',
-      network: 'base-sepolia',
-      nativeCurrency: {
-        decimals: 18,
-        name: 'Base Sepolia ETH',
-        symbol: 'ETH',
-      },
-      rpcUrls: {
-        default: { http: ['https://sepolia.base.org'] },
-      },
-      blockExplorers: {
-        default: { name: 'BaseScan', url: 'https://base-sepolia.blockscout.com' },
-      },
-      testnet: true,
-    },
-  ],
-  [jsonRpcProvider({ rpc: () => ({ http: 'https://sepolia.base.org' }) }), publicProvider()]
-);
-
-const { connectors } = getDefaultWallets({
-  appName: 'USDEC',
-  projectId: 'ced749b38222900677e11e8d3b875b2e',
-  chains,
-});
-
-const wagmiConfig = createConfig({
+const config = createConfig({
   autoConnect: true,
-  connectors,
-  publicClient,
+  publicClient: createPublicClient({
+    chain: baseSepolia,
+    transport: http()
+  })
 });
 
 export default function App({ Component, pageProps }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains}>
+    <WagmiProvider config={config}>
+      <RainbowKitProvider chains={[baseSepolia]}>
         <Component {...pageProps} />
       </RainbowKitProvider>
-    </WagmiConfig>
+    </WagmiProvider>
   );
 }
