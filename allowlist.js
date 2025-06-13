9c427277d8d6ae2884eb57593c97dde55c9d4e78
Charles <117126108+tokentrustadv@users.noreply.github.com>
2025-06-13 10:40:56 -0700

    Update allowlist.js

--- allowlist.js
+++ allowlist.js
@@ -1,5 +1,4 @@
 // allowlist.js
 export const allowedUsers = [
-  "0xEA8FD0B2C515Be4F5088687A323Cc9049de9FA1b".toLowerCase(), // You
-  // Add creators or paid subscribers here
+  '0xea8fd0b2c515be4f5088687a323cc9049de9fa1b' // You
 ];
